584b19d777aacf96759eeee5d4139f9566b24ee5
Nahuel Panigo <nahpanigo99@gmail.com>
2023-06-11 03:30:50 -0300

    adding frontend with TODO things

---
+++ front/src/components/wizardTestAndSelection.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="select variables">
+    <h2>{{this.$constants['STEPS'][2].label}}</h2>
+    <div id="variables-wrapper" class="boxDiv">
+        <div class="first-col">
+          <label for="criteriaSelect">Criteria</label>
+           <Multiselect class="select" id="criteriaSelect" v-model="criteriaVariable" :options="criteriaOptions" placeholder="criteria" ></Multiselect>
+          <div class="col">
+              <div class="check form-check form-switch">
+                  <input class="form-check-input" type="checkbox" id="ttestCheck" >
+                  <label class="form-check-label" for="ttestCheck"> Include ttest </label>
+              </div>
+          </div>  
+        </div>
+        <div class="second-col">
+                <div class="check form-check form-switch">
+                  <input class="form-check-input" type="checkbox" id="ztestCheck" >
+                  <label class="form-check-label" for="ztestCheck"> Include ztest </label>
+                </div>
+            <div class="col">
+                <div class="check form-check form-switch">
+                  <input class="form-check-input" type="checkbox" id="residualtestCheck" >
+                  <label class="form-check-label" for="residualtestCheck"> Include residual test </label>
+                </div>
+            </div>
+        </div>  
+  </div>
+  </div>
+</template>
+
+
+<script >
+
+import Multiselect from 'vue-multiselect'
+import {useModelSelectionStore} from '../stores/moldelSelection'
+import { watch,getCurrentInstance } from 'vue';
+
+
+
+
+export default {
+components: { Multiselect },
+data(){
+  return{
+      criteriaVariable: null,
+      criteriaOptions: Object.values(this.$constants['CRITERIA_OLS']),
+  }
+},
+setup(){
+  const modelSelectionStore = useModelSelectionStore();
+  const instance = getCurrentInstance();
+  console.log(modelSelectionStore.job)
+   /*watch(
+      () => modelSelectionStore.job.estimator,
+      (newValue) => {
+        instance.proxy.setSelectCriteriaOption(newValue)
+      }
+    );*/
+},
+methods:{
+    nextButton(){
+      if(this.criteriaVariable == null) {
+        return false
+      }else{
+        let ttest = document.getElementById("ttestCheck").checked
+        let ztest = document.getElementById("ztestCheck").checked
+        let residualtest = document.getElementById("residualtestCheck").checked
+        const modelSelectionStore = useModelSelectionStore();
+        modelSelectionStore.setTestAndSelectionData(this.criteriaVariable,ttest,ztest,residualtest)
+        return true
+      }
+    },
+    setSelectCriteriaOption(newValue){
+      const modelSelectionStore = useModelSelectionStore();
+      let criteria = 'CRITERIA_'+ newValue.toUpperCase()
+      this.criteriaVariable =  Object.values(this.$constants[criteria])
+    }
+    }
+  }
+</script>
+
+
+<style src="vue-multiselect/dist/vue-multiselect.css"></style>
+
+<style scoped>
+
+#variables-wrapper{
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
+}
+
+
+@media screen and (max-width: 450px) {
+#variables-wrapper{
+     display:initial;
+  }
+.check{
+  margin: 1rem;
+}
+}
+
+.check{
+  margin-top: 1.5rem;
+}
+.first-col{
+  grid-column: 1 / 2;
+}
+.second-col{
+  grid-column: 2/ 2;
+}
+.col{
+  margin-top: 1%;
+}
+.check{
+  padding-top: 2%;
+}
+</style>
---
+++ front/src/constants/index.js
@@ -0,0 +1,104 @@
+export default {
+    'API': {
+      'host': 'http://localhost:8000',
+      'paths': {
+        'server_info': '/server-info',
+        'load_database': '/upload-file',
+        'run': '/run',
+        'status' : '/status',
+        'summary': '/summary',
+        'results': '/download-results'
+      }
+    },
+    'INSAMPLE_MIN_SIZE': 20,
+    'STEPS': [
+      {
+        'label': 'Upload Data',
+        'icon': 'database',
+        'component': 'WizardUploadFile'
+      },
+      {
+        'label': 'Main Settings',
+        'icon': 'select',
+        'component': 'WizardSelectVar'
+      },
+      {
+        'label': 'Diagnostic tests and selection criteria',
+        'icon': 'tests',
+        'component': 'WizardTestAndSelection'
+      },
+      {
+        'label': 'Data cleaning and feature extraction',
+        'icon': 'settings',
+        'component': 'WizardSettings'
+      },
+      {
+        'label': 'Processing',
+        'icon': 'process',
+        'component': 'WizzardProcessing'
+      },
+      {
+        'label': 'Results',
+        'icon': 'results',
+        'component': 'WizardResults'
+      }
+    ],
+    'CRITERIA_OLS': {
+      'r2adj': 'Adjusted R²',
+      'bic': 'BIC',
+      'aic': 'AIC',
+      'aicc': 'AIC Corrected',
+      'cp': 'Mallows\'s Cp',
+      'rmse': 'RMSE',
+      'rmseout': 'RMSE OUT',
+      'sse': 'SSE'
+    },
+    'CRITERIA_LOGIT': {
+      'bic': 'BIC',
+      'aic': 'AIC',
+    },
+    'METHODS': [
+      'fast',
+      'precise'
+    ],
+    'PRELIMINARY_SELECTION':[
+      'lasso'
+    ],
+    'ESTIMATOR':[
+      'ols',
+      'logit'
+    ],
+    'JOB':{
+       1 :{
+        'estimator' : 'Estimator:',
+        'depvar' :'Dependent variable:',
+        'fixedvariables' : 'Fixed variables:',
+        'expvars' : 'Explanatory variables:',
+        'intercept': 'Include intercept:',
+        'method' : 'Calculation Precision:',
+        'time' : 'Time variable:',
+        'outsample' : 'Out-of-sample observations:'
+      },
+      2:{
+        'ttest' : 'Estimate ttest:',
+        'ztest' : 'Estimate ztest:',
+        'residualtest' : 'Estimate residualtest:',
+        'criteria': 'Ordering criteria:'
+      },
+      3:{
+        'fe_sqr': 'fe_sqr:', 
+        'fe_log': 'fe_log:', 
+        'fe_inv': 'fe_inv:', 
+        'fe_lag': 'fe_lag:', 
+        'interaction': 'interaction:',
+        'preliminaryselection': 'preliminaryselection:',
+        'modelavg': 'Display model averagin results:',
+        'orderresults': 'Sort results:',
+        'kfoldcrossvalidation': 'kfoldcrossvalidation:',
+        'numfolds' : 'numfolds:'
+      }
+    }
+    //proximamente deberiamos agregar estas opciones y sacar fast y precise
+    //"qr_64", "cho_64", "svd_64", "qr_32", "cho_32", "svd_32", "qr_16", "cho_16", "svd_16"
+  }
+  
