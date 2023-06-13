412a07f77c365fcdeac3ad97239e6c630a54d480
Adán Mauri Ungaro <adan.mauri@gmail.com>
2023-06-13 11:39:39 -0300

    Fixed tests files

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,9 @@ using ModelSelectionGUI
 const DOTENV_FILENAME = ".testenv"
 
 tests = [
-    #"variables/test_variables.jl",
-    #"server/test_server.jl",
-    #"files/test_files.jl",
-    "jobs/test_jobs.jl",
+    "variables/test_variables.jl",
+    "server/test_server.jl",
+    "files/test_files.jl",
 ]
 
 for test in tests
